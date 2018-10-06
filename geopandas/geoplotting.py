2afce99a20713621df0cf963e786de9e6cf3fb31
mazzma12 <matthieu.mazzolini@gmail.com>
2018-10-06 18:09:47 +0200

    Add animate methods

--- geopandas/geoplotting.py
+++ geopandas/geoplotting.py
@@ -89,10 +89,72 @@ def plot_markers_with_mapbox(gdf, text=None, center=None, zoom=None, size=None, 
     layout.mapbox['layers'] = layers
 
     fig = go.Figure(data=[trace], layout=layout)
-    iplot(fig)
+    if animate_by:
+        fig = animate(gdf, by=animate_by, fig=fig, text=text_col, opacity=opacity,
+                      size=size, color=color, mode=mode)
 
     if return_fig:
         return fig
+    iplot(fig)
+
+
+def animate(df, by, fig=None, **kwargs):
+    # Config for animate
+    play_pause_menus = [{'buttons': [{'args': [None, {'fromcurrent': True}],
+                                      'label': 'Play',
+                                      'method': 'animate'}],
+                         'direction': 'left',
+                         'pad': {'r': 20, 't': 40},
+                         'showactive': False,
+                         'type': 'buttons',
+                         'x': 0.1,
+                         'y': -0.1},
+                        {'buttons': [{'args': [[None],
+                                               {'frame': {'duration': 0, 'redraw': False},
+                                                'mode': 'immediate',
+                                                'transition': {'duration': 0}}],
+                                      'label': 'Pause',
+                                      'method': 'animate'}],
+                         'direction': 'left',
+                         'pad': {'r': 20, 't': 40},
+                         'showactive': False,
+                         'type': 'buttons',
+                         'x': 0.1,
+                         'y': 0.05}]
+    sliders = dict(currentvalue=dict(prefix='{}: '.format(', '.join([by])),
+                                     xanchor='right'),
+                   transition=dict(duration=300),
+                   pad=dict(b=10,
+                            t=30),
+                   len=0.9,
+                   x=0.1,
+                   steps=[])
+
+    frames = []
+    grouped = df.groupby(by, as_index=False)
+    # Compute once for all the groups
+    zoom, center = get_auto_zoom(df), get_auto_center(df)
+    for ii, (key, values) in enumerate(grouped):
+        group_fig = values.drop([by], 1).iplot(
+            return_fig=True, zoom=zoom, center=center, **kwargs)
+        frames.append({'data': group_fig['data'], 'name': str(key)})
+        step = dict(args=[[str(key)],
+                          dict(frame=dict(duration=300),
+                               mode='immediate',
+                               transition=dict(duration=300))],
+                    label=str(key),
+                    method='animate')
+        sliders['steps'].append(step)
+        if ii == 0 and fig is None:
+            # Init simple fig
+            fig = group_fig
+
+    # Add frames and config
+    fig['frames'] = frames
+    fig.layout.sliders = [sliders]
+    fig.layout.updatemenus = play_pause_menus
+
+    return fig
 
 
 def get_auto_center(gdf):
